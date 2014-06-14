309a07c577653b250a6bf1a7ba0d2fe31e22a8d4
Elliot N. Pearlman <epearls88@gmail.om>
2014-06-13 22:33:02 -0400

    comments added

--- src/DuplicateNumber.java
+++ src/DuplicateNumber.java
@@ -1,8 +1,3 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
-
 /*
 	Mre64
 	6/12/14
@@ -10,6 +5,9 @@ import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 	«The Internet is a great way to get on the net.»
 		- Bob Dole, Republican presidential candidate 
 */
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class DuplicateNumber {
 	public static ArrayList<Integer> holdValues = new ArrayList<Integer>();
 
@@ -17,49 +15,39 @@ public class DuplicateNumber {
 		// TODO Auto-generated method stub
 		holdValues.add(1);
 		holdValues.add(2);
-		holdValues.add(2);
+		holdValues.add(3);
 		holdValues.add(4);
 		holdValues.add(5);
 		holdValues.add(6);
 		holdValues.add(2);
-	
+		holdValues.add(8);
+		holdValues.add(2);
+		holdValues.add(10);
+		
 		Sort(holdValues);
 
 	}
 	static void Sort(ArrayList<Integer> input){
+		// create a copy of the ArrayList with a LinkedList
 		LinkedList<Integer> copy = new LinkedList<Integer>();
-		ArrayList<Integer> output = new ArrayList<Integer>();
-		
-		input = holdValues;
-		
+		// fill copy with origional contents
 		for(int i : holdValues){
 			copy.add(i);
 		}
-		//System.out.println(copy.toString());
+		/*
+		   we will be checking if copies exist by copying 
+		   the first element of the copy LinkedList, erasing 
+		   the first element, and checking the list against that.
+		*/
+		//ensure List is not empty while checking
 		while(!copy.isEmpty()){
-			System.out.println(copy.toString());
+			//copy value of first element
 			int store = copy.get(0);
+			//remove first element
 			copy.removeFirst();
-			for(int j = 0; j < 7; j++){
-				//int store = holdValues.get(j);
-				//System.out.print(holdValues.get(j));
-				
-
-//				System.out.println("remov " + copy.toString());
-//				System.out.println("remov " + store);
-				if(copy.contains(store)){
-					System.out.println(store);
-
-				}
-				//copy.remove(j);
-			}
-			
-			//copy.add(store);
-			//+System.out.println("after " + copy.toString());
-//			System.out.println("hold value: " + holdValues.get(j));
-//			System.out.println("store value: " + store);
-//			System.out.println("copy value: " + copy.get(j));
+			//check against store
+			if(copy.contains(store))
+				System.out.println(store);
 		}
 	}
-
 }
