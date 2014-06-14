f001a7e9fc9d421eabd5c1f9e50fc2f2eabc9446
Elliot N. Pearlman <epearls88@gmail.om>
2014-06-13 20:04:13 -0400

    zThe begining shell of the program

---
+++ src/DuplicateNumber.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/*
+	Mre64
+	6/12/14
+	
+	«The Internet is a great way to get on the net.»
+		- Bob Dole, Republican presidential candidate 
+*/
+public class DuplicateNumber {
+	private static ArrayList holdValues = new ArrayList();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Sort(holdValues);
+	}
+	static void Sort(ArrayList input){
+		input = holdValues;
+		
+	}
+
+}
